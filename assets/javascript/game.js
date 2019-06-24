61332515ef4a30144c71e541d5b15c41f95e4704
Ross <rossjameson1623@gmail.com>
2019-06-24 14:15:00 -0500

    got 1st button aka raiden to work

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var raiden = {
+    heroHealth: 100,
+    heroAttack: 10,
+    enemyHealth: 100,
+    enemyAttack: 10,
+}
+var scorpion = {
+    heroHealth: 100,
+    heroAttack: 10,
+    enemyHealth: 100,
+    enemyAttack: 10,
+}
+var sweetTooth = {
+    heroHealth: 100,
+    heroAttack: 10,
+    enemyHealth: 100,
+    enemyAttack: 10,
+}
+var liuKang = {
+    heroHealth: 100,
+    heroAttack: 10,
+    enemyHealth: 100,
+    enemyAttack: 10,
+}
+
+
+//Dom manipulation
+//find hero area to put 1st clicked character in
+var heroArea = document.getElementById('hero-area')
+// function for button click to pick characters
+function buttonPress(){
+    
+    heroArea.innerHTML = "<img src='Images/raiden.JPG' width=\'160px\' height=\'145px\'>";
+  
+}
+
+
+
+
