c8b68c3ed3c6031c9894119b5e359ee889ff571c
Ross <rossjameson1623@gmail.com>
2019-06-25 14:59:28 -0500

    added enemy characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,30 +28,52 @@ var liuKang = {
 //Dom manipulation
 //find hero area to put 1st clicked character in
 var heroArea = document.getElementById('hero-area');
+var enemyArea = document.getElementById('enemy-area')
 var heroStats = document.getElementById('hero-stats');
 var enemyStats = document.getElementById('enemy-stats');
 var showTextContent = enemyStats.textContent;
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("li").text("Enemy Health: 100" +" " + "Enemy Attack: 10");
-    });
-});
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("li").text("Enemy Health: 100" +" " + "Enemy Attack: 10");
+//     });
+// });
 // function for button click to pick characters and insert into hero area
 function buttonPress1(){
-    
     heroArea.innerHTML = "<img src='Images/raiden.JPG' width=\'170px\' height=\'145px\'>";
-   
-    
+        $("#enemy-health").css({opacity:'0.0'});
+        $("#enemy-attack").css({opacity:'0.0'});
+        $("#raiden").css({opacity:'0.0'});
+        $("#scorpion").css({opacity:'0.0'});
+        $("#liukang").css({opacity:'0.0'});
+        $("#sweettooth").css({opacity:'0.0'})
 }
 function buttonPress2(){
-    heroArea.innerHTML = "<img src='Images/sweetTooth.JPG' width=\'170px\' height=\'145px\'>";
+heroArea.innerHTML = "<img src='Images/sweetTooth.JPG' width=\'170px\' height=\'145px\'>";
+    $("#enemy-health").css({opacity:'0.0'});
+    $("#enemy-attack").css({opacity:'0.0'});
+    $("#sweettooth").css({opacity:'0.0'});
+    $("#raiden").css({opacity:'1.0'});
+    $("#scorpion").css({opacity:'1.0'});
+    $("#liukang").css({opacity:'1.0'}); 
 }
 function buttonPress3(){
     heroArea.innerHTML = "<img src='Images/scorpion.JPG' width=\'170px\' height=\'145px\'>";
+    $("#enemy-health").css({opacity:'0.0'});
+    $("#enemy-attack").css({opacity:'0.0'});
+    $("#scorpion").css({opacity:'0.0'});
+    $("#liukang").css({opacity:'1.0'});
+    $("#sweettooth").css({opacity:'1.0'});
+    $("#raiden").css({opacity:'1.0'});
 }
 function buttonPress4(){
     heroArea.innerHTML = "<img src='Images/liuKang.JPG' width=\'170px\' height=\'145px\'>";
+    $("#enemy-health").css({opacity:'0.0'});
+    $("#enemy-attack").css({opacity:'0.0'});
+    $("#liukang").css({opacity:'0.0'});
+    $("#sweettooth").css({opacity:'1.0'});
+    $("#raiden").css({opacity:'1.0'});
+    $("#scorpion").css({opacity:'1.0'});
 }
 // if(buttonPress1()){
 //     heroArea.
