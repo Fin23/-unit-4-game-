9203995721aacfead3746b40522a592319b55549
Ross <rossjameson1623@gmail.com>
2019-06-24 23:53:17 -0500

    adding button function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,48 @@
 var raiden = {
-    heroHealth: 100,
-    heroAttack: 10,
+    raidenHealth: 100,
+    raidenAttack: 10,
     enemyHealth: 100,
     enemyAttack: 10,
 }
 var scorpion = {
-    heroHealth: 100,
-    heroAttack: 10,
+    scorpionHealth: 100,
+    scorpionAttack: 10,
     enemyHealth: 100,
     enemyAttack: 10,
 }
 var sweetTooth = {
-    heroHealth: 100,
-    heroAttack: 10,
+    sweetToothHealth: 100,
+    sweetToothAttack: 10,
     enemyHealth: 100,
     enemyAttack: 10,
 }
 var liuKang = {
-    heroHealth: 100,
-    heroAttack: 10,
+    liuKangHealth: 100,
+    liuKangAttack: 10,
     enemyHealth: 100,
     enemyAttack: 10,
 }
 
 
+
 //Dom manipulation
 //find hero area to put 1st clicked character in
-var heroArea = document.getElementById('hero-area')
-// function for button click to pick characters
+var heroArea = document.getElementById('hero-area');
+var heroStats = document.getElementById('hero-stats');
+var enemyStats = document.getElementById('enemy-stats');
+var showTextContent = enemyStats.textContent;
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("li").text("Enemy Health: 100" +" " + "Enemy Attack: 10");
+    });
+});
+// function for button click to pick characters and insert into hero area
 function buttonPress1(){
+    
     heroArea.innerHTML = "<img src='Images/raiden.JPG' width=\'170px\' height=\'145px\'>";
+   
+    
 }
 function buttonPress2(){
     heroArea.innerHTML = "<img src='Images/sweetTooth.JPG' width=\'170px\' height=\'145px\'>";
@@ -40,6 +53,10 @@ function buttonPress3(){
 function buttonPress4(){
     heroArea.innerHTML = "<img src='Images/liuKang.JPG' width=\'170px\' height=\'145px\'>";
 }
+// if(buttonPress1()){
+//     heroArea.
+// }
+
 
 
 
